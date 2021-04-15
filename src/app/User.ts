12b7475d29fb8300c21178e30fac7ed6a418d39d
unknown <ibmjavanh14@iiht.tech>
2021-04-15 13:43:34 +0530

    first commit

---
+++ src/app/User.ts
@@ -0,0 +1,4 @@
+export class User{
+firstname:string='Ram';
+age:number=20;
+}
---
+++ src/app/userform/userform.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { User } from '../User';
+
+@Component({
+  selector: 'app-userform',
+  templateUrl: './userform.component.html',
+  styleUrls: ['./userform.component.css']
+})
+export class UserformComponent implements OnInit {//controller
+  title:string='Userform';
+  user:User=new User();//model-stores all form data
+  userArray:User[]=[];
+  constructor() { }
+  save(){
+    this.userArray.push(this.user);
+    console.log("working...");
+    console.log(this.user.firstname);
+    this.user.firstname='John';
+  }
+
+  ngOnInit(): void {
+  }
+
+}
